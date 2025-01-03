eecae94672feae48c06f56eb58d94ff1ed258472
Matteo Cirami <mcirami@users.noreply.github.com>
2025-01-03 16:20:15 -0600

    adjusted poloaris styling

--- web/routes/index.jsx
+++ web/routes/index.jsx
@@ -6,18 +6,15 @@ import {
     Page,
     Spinner,
     Text,
-    InlineGrid,
-    Grid,
     AccountConnection,
     List,
     Button,
-    Icon,
     Link,
-    Box,
     Divider,
     InlineStack,
-    ExceptionList,
     BlockStack,
+    MediaCard,
+    Icon,
 } from "@shopify/polaris";
 import { StoreIcon, IconsIcon } from "@shopify/polaris-icons";
 import { api } from "../api";
@@ -40,7 +37,7 @@ const gadgetMetaQuery = `
   }
 `;
 
-export default function () {
+export default function Index() {
     const [{ data, fetching, error }] = useFindFirst(api.shopifyShop);
     const [{ data: metaData, fetching: fetchingGadgetMeta }] = useQuery({
         query: gadgetMetaQuery,
@@ -55,9 +52,11 @@ export default function () {
 
     const handleConnect = useCallback(() => {
         window.open(
-            hostUrl + "/connect-shopify-store?domain=" + encodeURI(storeDomain)
+            `${hostUrl}/connect-shopify-store?domain=${encodeURIComponent(
+                storeDomain
+            )}`
         );
-    }, []);
+    }, [hostUrl, storeDomain]);
 
     const handleDisconnect = useCallback(() => {
         getStoreRecords(storeDomain).then((records) => {
@@ -68,7 +67,7 @@ export default function () {
                 }
             });
         });
-    }, []);
+    }, [storeDomain]);
 
     useEffect(() => {
         getStoreRecords(storeDomain).then((records) => {
@@ -77,7 +76,7 @@ export default function () {
                 setConnected(true);
             }
         });
-    }, []);
+    }, [storeDomain]);
 
     const buttonText = connected
         ? "Disconnect"
@@ -87,7 +86,7 @@ export default function () {
     if (error) {
         return (
             <Page title="Error">
-                <Text variant="bodyMd" as="p">
+                <Text as="p" variant="bodyMd">
                     Error: {error.toString()}
                 </Text>
             </Page>
@@ -96,170 +95,130 @@ export default function () {
 
     if (fetching || fetchingGadgetMeta) {
         return (
+            <Page>
+                <BlockStack alignment="center" spacing="loose">
+                    <Spinner accessibilityLabel="Loading" size="large" />
+                </BlockStack>
+            </Page>
+        );
+    }
+
+    const Placeholder = ({ text, icon }) => {
+        return (
             <div
                 style={{
                     display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100%",
-                    width: "100%",
+                    justifyContent: "start",
+                    gap: "1rem",
                 }}
             >
-                <Spinner accessibilityLabel="Spinner example" size="large" />
+                <Icon source={icon} />
+
+                <Text>{text}</Text>
             </div>
+        );
+    };
+
+    function CardContent() {
+        return (
+            <>
+                {connected ? (
+                    <InlineStack gap="400">
+                        <Text as="h4" variant="headingMd">
+                            How to add products to your LinkPro page:
+                        </Text>
+                        <List type="number" gap="loose">
+                            <List.Item>
+                                <Link url="https://link.pro/" target="_blank">
+                                    Go To Link.pro
+                                </Link>
+                            </List.Item>
+                            <List.Item>
+                                After logging in, go to the "Pages" page
+                            </List.Item>
+                            <List.Item>Click "Add Icon"</List.Item>
+                            <List.Item>
+                                Select the Integrations tab and choose "Shopify"
+                                from the dropdown list.
+                            </List.Item>
+                            <List.Item>
+                                Select your store and the products you want to
+                                display.
+                            </List.Item>
+                            <List.Item>Add a link name.</List.Item>
+                            <List.Item>
+                                Click "SAVE" and you are all set!
+                            </List.Item>
+                        </List>
+                        <BlockStack gap="400" wrap="false">
+                            <Text as="p" variant="bodyMd">
+                                Your store is currently connected to LinkPro{" "}
+                            </Text>
+                            <Button
+                                onClick={handleDisconnect}
+                                size="slim"
+                                external
+                            >
+                                {buttonText}
+                            </Button>
+                        </BlockStack>
+                    </InlineStack>
+                ) : (
+                    <InlineStack gap="400">
+                        <Text as="h3" variant="headingLg">
+                            Your LinkPro Shop
+                        </Text>
+                        <Placeholder
+                            text="Enable people to shop your most popular products
+                                right on your LinkPro page."
+                            icon={StoreIcon}
+                        />
+                        <Placeholder
+                            text="Build your brand by sharing your latest content and events, grow your subscriber list, and more."
+                            icon={IconsIcon}
+                        />
+                        <Placeholder
+                            text="Create your own style by uploading your own icons and images to display."
+                            icon={IconsIcon}
+                        />
+                        <Button
+                            onClick={handleConnect}
+                            variant="primary"
+                            size="slim"
+                            external
+                        >
+                            {buttonText}
+                        </Button>
+                        <Text as="p" variant="bodyMd">
+                            By connecting your account, you agree to accept{" "}
+                            <Link
+                                url="https://link.pro/terms-and-conditions"
+                                target="_blank"
+                            >
+                                LinkPro's Terms and Conditions
+                            </Link>
+                        </Text>
+                    </InlineStack>
+                )}
+            </>
         );
     }
 
     return (
         <Page title="LinkPro">
             <Layout>
-                {connected ? (
-                    <>
-                        <Layout.Section>
-                            <Card>
-                                <Box paddingBlockEnd="400">
-                                    <Text variant="heading2xl" as="h2">
-                                        Boost discovery of your products and
-                                        streamline shopping with LinkPro!
-                                    </Text>
-                                </Box>
-                                <Box paddingBlockEnd="200">
-                                    <Text variant="headingLg" as="h3">
-                                        Enable people to shop your most popular
-                                        products right on your LinkPro page!
-                                    </Text>
-                                </Box>
-                                <Box
-                                    paddingBlockEnd="400"
-                                    paddingBlockStart="400"
-                                >
-                                    <Divider />
-                                </Box>
-                                <Box paddingBlockStart="400">
-                                    <Text variant="headingMd" as="h4">
-                                        How to add products to your LinkPro
-                                        page:
-                                    </Text>
-                                </Box>
-                                <Box
-                                    paddingBlockStart="400"
-                                    paddingBlockEnd="400"
-                                >
-                                    <List type="number">
-                                        <List.Item>
-                                            Click "Go To LinkPro" button below
-                                        </List.Item>
-                                        <List.Item>
-                                            After logging in, Go to "Pages" page
-                                        </List.Item>
-                                        <List.Item>Click "Add Icon"</List.Item>
-                                        <List.Item>
-                                            Select Integrations tab and choose
-                                            "Shopify" from the dropdown list.
-                                        </List.Item>
-                                        <List.Item>
-                                            Select your store and select the
-                                            products you want to display.
-                                        </List.Item>
-                                        <List.Item>Add a link name.</List.Item>
-                                        <List.Item>
-                                            Click "SAVE" and you are all set!
-                                        </List.Item>
-                                    </List>
-                                </Box>
-                                <Button
-                                    variant="primary"
-                                    fullWidth
-                                    url="https://link.pro"
-                                    external
-                                    size="large"
-                                >
-                                    Go To LinkPro
-                                </Button>
-                            </Card>
-                        </Layout.Section>
-                        <Layout.Section>
-                            <AccountConnection
-                                accountName={accountName}
-                                connected={connected}
-                                title="LinkPro"
-                                avatarUrl="https://d2qqgh4ebru6pi.cloudfront.net/ce41833e-bc7e-4017-bc67-2abe5b6a2987/images/preview-device-bg.png"
-                                action={{
-                                    content: buttonText,
-                                    onAction: handleDisconnect,
-                                }}
-                                details={details}
-                            />
-                        </Layout.Section>
-                    </>
-                ) : (
-                    <Layout.Section>
-                        <Card>
-                            <InlineGrid columns={["twoThirds", "oneThird"]}>
-                                <BlockStack gap="200">
-                                    <Text variant="heading2xl" as="h2">
-                                        Boost discovery of your products and
-                                        streamline shopping with LinkPro
-                                    </Text>
-
-                                    <ExceptionList
-                                        items={[
-                                            {
-                                                icon: StoreIcon,
-                                                description:
-                                                    "Enable people to shop your most popular products right on your LinkPro page.",
-                                            },
-                                            {
-                                                icon: IconsIcon,
-                                                description:
-                                                    " Build your brand by sharing your latest content and events, grow your subscriber list, and more",
-                                            },
-                                        ]}
-                                    />
-                                    <Divider borderColor="transparent" />
-                                    <Box>
-                                        <AccountConnection
-                                            accountName={accountName}
-                                            connected={connected}
-                                            title="LinkPro"
-                                            avatarUrl="https://d2qqgh4ebru6pi.cloudfront.net/ce41833e-bc7e-4017-bc67-2abe5b6a2987/images/preview-device-bg.png"
-                                            action={{
-                                                content: buttonText,
-                                                onAction: handleConnect,
-                                            }}
-                                            details={details}
-                                        />
-                                    </Box>
-                                    <Divider borderColor="transparent" />
-                                    {!connected && (
-                                        <Box>
-                                            <Text variant="bodyMd" as="p">
-                                                Don't have an account yet?
-                                            </Text>
-                                            <Link
-                                                url={`${hostUrl}/register`}
-                                                external
-                                            >
-                                                Click Here To Create a LinkPro
-                                                Account
-                                            </Link>
-                                        </Box>
-                                    )}
-                                </BlockStack>
-                                <Box>
-                                    <img
-                                        style={{
-                                            width: "100%",
-                                            maxWidth: "200px",
-                                            margin: "0 auto",
-                                        }}
-                                        src="https://d2qqgh4ebru6pi.cloudfront.net/ce41833e-bc7e-4017-bc67-2abe5b6a2987/images/img-phone.png"
-                                    />
-                                </Box>
-                            </InlineGrid>
-                        </Card>
-                    </Layout.Section>
-                )}
+                <Layout.Section>
+                    <MediaCard description={<CardContent />}>
+                        <img
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                            }}
+                            src="https://lp-production-images.s3.us-east-2.amazonaws.com/shopify-image-final.png"
+                            alt="LinkPro example"
+                        />
+                    </MediaCard>
+                </Layout.Section>
             </Layout>
         </Page>
     );
